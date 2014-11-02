16d6921041104ed8dd581e968f588ca30e1dd76f
Michael McMillan <email@michaelmcmillan.net>
2014-11-02 20:10:10 +0100

    added controller for the index page and for a single job

--- app/controllers/board.php
+++ app/controllers/board.php
@@ -2,10 +2,17 @@
 class Board {
 
     public function index () {
-        echo 'hey!';
+        $app = \Slim\Slim::getInstance();
+        $app->render('header.php');
+        $app->render('board.php');
+        $app->render('footer.php');
     }
 
     public function job ($id) {
+        $app = \Slim\Slim::getInstance();
 
+        $x = Job::find(1);
+
+        print_r($x);
     }
 }
