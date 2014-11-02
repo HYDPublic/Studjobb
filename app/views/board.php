88dd55a2df6f8d862afead4e6212a83a2f50b9ba
Michael McMillan <email@michaelmcmillan.net>
2014-11-02 20:09:16 +0100

    added dir for views

---
+++ app/views/board.php
@@ -0,0 +1,33 @@
+        <div id="board" class="col-md-8 col-md-offset-2">
+
+            <div class="intro">
+                <p>
+                    <strong>Studjobb.no</strong> er nettsiden for studenter som
+                    ønsker å finne jobb, prosjekter og oppdrag. Kom i kontakt med
+                    bedrifter som ønsker å ansette studenter!
+                </p>
+                <p>
+                    <strong>Ikke gå glipp av drømmejobben!</strong> Hver mandag
+                    sender vi deg aktuelle stillinger på e-post. Klikk her for å
+                    få med deg jobber som passer for deg! Du kan også følge @Studjobb
+                    på.
+                </p>
+            </div>
+
+            <div class="search well col-md-12">
+                <div class="col-md-5">
+                    <input type="text" class="form-control"
+                        placeholder="Søk 'trondheim', 'web-utvikling' ...">
+                </div>
+                <div class="col-md-2 col-md-offset-4">
+                    <button type="button" class="btn btn-custom-lighten">
+                        Utlys stilling!
+                    </button>
+                </div>
+            </div>
+
+            <div class="listing">
+
+            </div>
+
+        </div>
---
+++ app/views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Studjobb</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div id="header">
+            <video autoplay loop poster="video/video.jpg">
+			    <source src="video/ntnu3.mp4" type="video/mp4" />
+			   
+		    </video>
+        </div>
