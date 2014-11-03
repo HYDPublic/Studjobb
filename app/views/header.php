7bc914e25287ac271f10b2c8bdbefdeeaccfee4d
Michael McMillan <email@michaelmcmillan.net>
2014-11-03 15:14:36 +0100

    add app.js and jquery to header

--- app/views/header.php
+++ app/views/header.php
@@ -7,7 +7,9 @@
         <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="/css/font-awesome.min.css" >
         <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/jquery.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/app.js"></script>
     </head>
     <body>
         <div id="header">
