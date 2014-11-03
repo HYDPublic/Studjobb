f14a1dfcbb9bea00028887fbde14f1e51692dacb
Michael McMillan <email@michaelmcmillan.net>
2014-11-03 09:56:50 +0100

    added footer text and links

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,2 +1,11 @@
+
+        <div id="footer" class="col-md-8 col-md-offset-2">
+            <div class="col-md-6">
+                <p>&copy; Studjobb 2014. Org.nummer: 997452364</p>
+            </div>
+            <div class="col-md-6 text-right">
+                <b><a href="/om">Om Studjobb</a></b>
+            </div>
+        </div>
     </body>
 </html>
