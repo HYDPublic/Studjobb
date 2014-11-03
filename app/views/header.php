09facf2c148e8c745b9239a43689f5776fc59b3c
Michael McMillan <email@michaelmcmillan.net>
2014-11-03 14:31:00 +0100

    urls point to the root for assets

--- app/views/header.php
+++ app/views/header.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <title>Studjobb</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/font-awesome.min.css" >
-        <link rel="stylesheet" href="css/style.css">
-        <script src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/css/font-awesome.min.css" >
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div id="header">
