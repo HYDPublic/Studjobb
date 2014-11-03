238b629ba6ab02d6eb0efe9f97cb631a41500e31
Michael McMillan <email@michaelmcmillan.net>
2014-11-03 22:00:34 +0100

    moved static views to static dir

---
+++ app/views/static/notFound.php
@@ -0,0 +1,9 @@
+        <div id="notfound" class="col-md-8 col-md-offset-2">
+
+            <div class="intro">
+                <h3>Ooops!</h3>
+                <p>
+                    Vi finner ikke siden du leter etter.
+                </p>
+            </div>
+        </div>
