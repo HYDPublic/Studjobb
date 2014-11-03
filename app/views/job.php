249e89ff94430b6c11401d01e882d81c18cf68e8
Michael McMillan <email@michaelmcmillan.net>
2014-11-03 15:25:06 +0100

    fill in job page with the real data

--- app/views/job.php
+++ app/views/job.php
@@ -1,9 +1,13 @@
         <div id="job" class="col-md-8 col-md-offset-2">
 
-            <div class="intro">
-                <h3>Jobb</h3>
-                <p>
-                    Jobb-dummy
-                </p>
+            <div class="intro col-md-8">
+                <h3><?php print $job->title; ?></h3>
+                <div class="content">
+                    <?php print $job->content; ?>
+                </div>
+            </div>
+
+            <div class="company col-md-4 well">
+                <h4><?php print $job->company->name; ?></h4>
             </div>
         </div>
