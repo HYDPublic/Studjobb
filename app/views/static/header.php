035c2b44a066be6d3699f1d49f929a5b4afcb532
Michael McMillan <email@michaelmcmillan.net>
2014-11-04 09:18:28 +0100

    timeago inclusion

--- app/views/static/header.php
+++ app/views/static/header.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" href="/css/font-awesome.min.css" >
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/jquery.min.js"></script>
+        <script src="/js/timeago.jquery.js"></script>
+        <script src="/js/no-nb.timeago.js"></script>
         <script src="/js/bootstrap.min.js"></script>
         <script src="/js/quill.min.js"></script>
         <script src="/js/app.js"></script>
