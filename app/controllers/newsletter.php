86d65ca907137c9c0a1b64c4e723f1e5e20cb594
Michael McMillan <email@michaelmcmillan.net>
2014-11-04 13:32:53 +0100

    send_welcome email on signup

--- app/controllers/newsletter.php
+++ app/controllers/newsletter.php
@@ -17,7 +17,7 @@ class Newsletter {
             'double_optin'      => false,
             'update_existing'   => true,
             'replace_interests' => false,
-            'send_welcome'      => false,
+            'send_welcome'      => true,
         ));
 
         if (isset($query['status']) && $query['status'] == 'error')
