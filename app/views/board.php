0ebdddcde726d95e0565051698caa50116195271
Michael McMillan <email@michaelmcmillan.net>
2014-11-04 09:59:51 +0100

    filter unpublished jobs

--- app/views/board.php
+++ app/views/board.php
@@ -35,6 +35,7 @@
                     <table class="table table-hover">
                         <tbody>
                             <?php foreach ($category->jobs as $job): ?>
+                            <?php if ($job->published == true): ?>
                             <tr data-href="/stilling/<?php print $job->id; ?>">
                                 <td class="company"><strong><?php print $job->company->name; ?></strong></td>
                                 <td class="type"><span class="badge badge-orange"><?php print $job->type; ?></span></td>
@@ -42,6 +43,7 @@
                                 <td class="place"><?php print $job->place; ?></td>
                                 <td class="due timeago" title="<?php print $job->due(); ?>"></td>
                             </tr>
+                            <?php endif; ?>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
