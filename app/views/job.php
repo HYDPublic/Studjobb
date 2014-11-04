41818ea0890b8e7d6ebfb88d2915d4009b4e4c56
Michael McMillan <email@michaelmcmillan.net>
2014-11-04 09:58:44 +0100

    rearranged the about company section issue #2 fix

--- app/views/job.php
+++ app/views/job.php
@@ -1,6 +1,6 @@
         <div id="job" class="col-md-8 col-md-offset-2">
 
-            <div class="intro col-md-8">
+            <div class="intro col-md-10">
                 <p>
                     <a class="back" href="/">← Tilbake til forsiden</a>
                 <?php if (Sentry::check()): ?>
@@ -11,15 +11,16 @@
                 </p>
 
                 <h3><?php print $job->title; ?></h3>
+
+                <div class="company well">
+                    <img src="<?php print $job->company->logo; ?>" class="logo">
+                    <div class="about">
+                        <p><?php print $job->company->about; ?></p>
+                    </div>
+                </div>
+
                 <div class="content">
                     <?php print $job->content; ?>
-                </div>
-            </div>
-
-            <div class="company col-md-4 well">
-                <img src="<?php print $job->company->logo; ?>" class="logo">
-                <div class="about">
-                    <p><?php print $job->company->about; ?></p>
                 </div>
             </div>
         </div>
