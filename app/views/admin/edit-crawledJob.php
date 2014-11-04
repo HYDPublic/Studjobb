33f14cfca8b3bc56efb34a0ba5c2eeb2dd81404f
Michael McMillan <email@michaelmcmillan.net>
2014-11-04 17:21:38 +0100

    added email section to crawledjob

--- app/views/admin/edit-crawledJob.php
+++ app/views/admin/edit-crawledJob.php
@@ -1,70 +1,92 @@
         <div id="job" class="col-md-8 col-md-offset-2">
 
             <div class="intro col-md-12">
+                <div class="row">
+                    <div class="col-md-10">
+                        <p>
+                            <a class="back" href="/admin/dashbord">
+                                ← Tilbake til Dashbordet
+                            </a>
+                        </p>
 
-                <p>
-                    <a class="back" href="/admin/dashbord">
-                        ← Tilbake til Dashbordet
-                    </a>
-                </p>
+                        <p>
+                            <a href="<?php print $crawledJob->url; ?>" target="_blank">
+                                <strong>Besøk kilden</strong>
+                                <span class="glyphicon glyphicon-share-alt"></span>
+                            </a>
+                        </p>
+                        <p>
+                            <label>Status: </label>
+                            <select>
+                                <option value="volvo">Ingenting</option>
+                                <option value="volvo">Sendt mail</option>
+                                <option value="volvo">Bestilt</option>
+                                <option value="volvo">Sendt faktura</option>
+                                <option value="volvo">Betalt</option>
+                            </select>
+                            <br/>
 
-                <p>
-                    <a href="<?php print $crawledJob->url; ?>" target="_blank">
-                        <strong>Besøk kilden</strong>
-                        <span class="glyphicon glyphicon-share-alt"></span>
-                    </a>
-                </p>
-                <p>
-                    <label>Status: </label>
-                    <select>
-                        <option value="volvo">Ingenting</option>
-                        <option value="volvo">Sendt mail</option>
-                        <option value="volvo">Bestilt</option>
-                        <option value="volvo">Sendt faktura</option>
-                        <option value="volvo">Betalt</option>
-                    </select>
-                    <br/>
+                            <label>Skrapt den: </label>
+                            <span><?php print $crawledJob->createdAt(); ?></span>
+                            <br/>
 
-                    <label>Skrapt den: </label>
-                    <span><?php print $crawledJob->createdAt(); ?></span>
-                    <br/>
-
-                    <label>Selskap: </label>
-                    <span><?php print $crawledJob->company; ?></span>
-                </p>
+                            <label>Selskap: </label>
+                            <span><?php print $crawledJob->company; ?></span>
+                        </p>
+                    </div>
+                </div>
 
                 <hr>
 
-                <div class="col-md-10">
-                    <form id="update-job" method="post" action="/admin/stilling">
-                        <input type="text" name="title" class="form-control" value="<?php print $crawledJob->title; ?>">
+                <div class="row">
+                    <h3>Send e-post</h3>
+                    <div class="col-md-10">
+                        <form id="mail" method="post" action="/admin/mail">
+                            <input type="text" name="to" class="form-control" placeholder="E-post"
+                                value="<?php print $crawledJob->contact; ?>">
+                            <input type="text" name="name" class="form-control" placeholder="Navn">
+                            <input type="text" name="subject" class="form-control" value="Utlysning på Studjobb.no">
+                            <textarea class="form-control" name="body"></textarea>
+                            <br/>
+                            <input type="submit" class="btn btn-custom-lighten" value="Send mail">
+                        </form>
+                    </div>
+                </div>
 
-                        <div id="formatting-container">
-                            <select title="Size" class="ql-size">
-                                <option value="10px">Small</option>
-                                <option value="13px" selected>Normal</option>
-                                <option value="18px">Large</option>
-                                <option value="32px">Huge</option>
-                            </select>
+                <hr>
 
-                            <button type="button" class="btn ql-bold">Bold</button>
-                            <button type="button" class="btn ql-italic">Italic</button>
-                            <button type="button" class="btn ql-underline">Under</button>
-                            <button type="button" class="btn ql-strike">Strike</button>
-                            <button type="button" class="btn ql-link">Link</button>
-                            <button type="button" class="btn ql-image">Image</button>
-                            <button type="button" class="btn ql-bullet">Bullet</button>
-                            <button type="button" class="btn ql-list">List</button>
-                        </div>
+                <div class="row">
+                    <h3>Utlysning</h3>
+                    <div class="col-md-10">
+                        <form id="update-job" method="post" action="/admin/stilling">
+                            <input type="text" name="title" class="form-control" value="<?php print $crawledJob->title; ?>">
 
-                        <div id="editor-container"><?php print $crawledJob->content; ?></div>
+                            <div id="formatting-container">
+                                <select title="Size" class="ql-size">
+                                    <option value="10px">Small</option>
+                                    <option value="13px" selected>Normal</option>
+                                    <option value="18px">Large</option>
+                                    <option value="32px">Huge</option>
+                                </select>
 
-                        <textarea id="buffer" name="content"></textarea>
+                                <button type="button" class="btn ql-bold">Bold</button>
+                                <button type="button" class="btn ql-italic">Italic</button>
+                                <button type="button" class="btn ql-underline">Under</button>
+                                <button type="button" class="btn ql-strike">Strike</button>
+                                <button type="button" class="btn ql-link">Link</button>
+                                <button type="button" class="btn ql-image">Image</button>
+                                <button type="button" class="btn ql-bullet">Bullet</button>
+                                <button type="button" class="btn ql-list">List</button>
+                            </div>
 
-                        <input type="submit" class="btn btn-custom-lighten" value="Publiser">
-                    </form>
+                            <div id="editor-container"><?php print $crawledJob->content; ?></div>
 
+                            <textarea id="buffer" name="content"></textarea>
+
+                            <input type="submit" class="btn btn-custom-lighten" value="Publiser">
+                        </form>
+
+                    </div>
                 </div>
             </div>
-
         </div>
