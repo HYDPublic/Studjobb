9ffacaa33d7b274822c187addcf6f06f8b974178
Michael McMillan <email@michaelmcmillan.net>
2014-11-06 20:15:02 +0100

    re-arranged tables on admin

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,35 +1,48 @@
-        <div id="dashboard" class="col-md-8 col-md-offset-2">
+        <div id="dashboard" class="col-md-12">
 
             <div class="intro">
+
                 <h3>Dashbord</h3>
                 <h4>Medlemmer på nyhetsbrevet: <?php print $count; ?></h4>
-                <p>Dette er alle jobbene i systemet. Klikk for å redigere.</p>
-                <table class="table table-hover table-jobs">
-                    <tbody>
-                        <?php foreach ($jobs as $job): ?>
-                        <tr data-href="/admin/stilling/<?php print $job->id; ?>">
-                            <td><strong><?php print $job->company->name; ?></strong></td>
-                            <td><span class="badge badge-orange"><?php print $job->type; ?></span></td>
-                            <td><span class="job-title"><?php print $job->title; ?></span></td>
-                            <td><?php print $job->place; ?></td>
-                            <td>Frist: <?php print $job->due(); ?></td>
-                            <td><?php if ($job->published) print 'Publisert'; else print 'Upublisert'; ?></td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
 
-                <h3>Selskaper</h3>
-                <p>Dette er alle selskapene.</p>
-                <table class="table table-hover table-jobs">
-                    <tbody>
-                        <?php foreach ($companies as $company): ?>
-                        <tr data-href="/admin/selskap/<?php print $company->id; ?>">
-                            <td><strong><?php print $company->name; ?></strong></td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3>Jobber i systemet</h3>
+                        <p>Dette er alle jobbene i systemet. Klikk for å redigere.</p>
+                        <table class="table table-hover table-jobs">
+                            <tbody>
+                                <?php foreach ($jobs as $job): ?>
+                                <tr data-href="/admin/stilling/<?php print $job->id; ?>">
+                                    <td><strong><?php print $job->company->name; ?></strong></td>
+                                    <td><span class="badge badge-orange"><?php print $job->type; ?></span></td>
+                                    <td><span class="job-title"><?php print $job->title; ?></span></td>
+                                    <td><?php print $job->place; ?></td>
+                                    <td>Frist: <?php print $job->due(); ?></td>
+                                    <td><?php if ($job->published) print 'Publisert'; else print 'Upublisert'; ?></td>
+                                </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="col-md-6">
+                        <h3>Selskaper</h3>
+                        <p>Dette er alle selskapene.</p>
+                        <table class="table table-hover table-jobs">
+                            <tbody>
+                                <?php foreach ($companies as $company): ?>
+                                <tr data-href="/admin/selskap/<?php print $company->id; ?>">
+                                    <td><strong><?php print $company->name; ?></strong></td>
+                                </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
 
                 <h3>Skrapt</h3>
                 <p>Dette er de skrapte jobbene.</p>
