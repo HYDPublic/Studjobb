1f10b491479dbf03c6b08a676f6b9b0b95b62f2c
Michael McMillan <email@michaelmcmillan.net>
2014-11-06 18:03:48 +0100

    added opengraph tags and corresponding image

--- app/views/static/header.php
+++ app/views/static/header.php
@@ -3,6 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <title>Studjobb</title>
+        <meta property="og:title" content="Studjobb">
+        <meta property="og:description" content="Nettstedet for studenter som ønsker å finne jobb, startups og oppdrag.">
+        <meta property="og:url" content="http://studjobb.no">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="http://studjobb.no/img/og.jpg">
         <link rel="icon" type="image/png" href="/favicon.ico">
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
