70b99b7049d6237a4ca6cf6f003b399a5388839c
Michael McMillan <email@michaelmcmillan.net>
2014-11-07 20:57:31 +0100

    view for submitting a job

---
+++ app/views/submit.php
@@ -0,0 +1,75 @@
+
+
+        <div id="board" class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+            <div class="row">
+                <div class="intro">
+                    <p>
+                        Annonsen din vil være synlig på forsiden i 30 dager. Etter
+                        det vil den bli arkivert.
+                    </p><br/>
+                </div>
+            </div>
+
+            <form class="form-horizontal" role="form">
+                <legend>Steg 1: Stillingen</legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Stillingstittel</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control">
+                        <p class="help-block">For eksempel 'systemutvikler', 'trainee' eller 'sommerjobb'.</p>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Sted</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control">
+                        <p class="help-block">Hvor i landet gjelder stillingen?</p>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Beskrivelse</label>
+                    <div class="col-sm-8">
+                        <div id="formatting-container">
+                            <button type="button" class="btn ql-bold">Fet</button>
+                            <button type="button" class="btn ql-italic">Kursiv</button>
+                            <button type="button" class="btn ql-underline">Understrek</button>
+                            <button type="button" class="btn ql-link">Lenke</button>
+                            <button type="button" class="btn ql-bullet">Liste</button>
+                        </div>
+                        <div id="editor-container"></div>
+                        <p class="help-block">
+                            Selve stillingsbeskrivelsen. Husk å skrive hvordan
+                            man skal søke og hvem man skal ta kontakt med.
+                        </p>
+                    </div>
+                </div>
+
+                <legend>Steg 2: Selskapet</legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Navn</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control">
+                        <p class="help-block">Navn på selskapet.</p>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Logo</label>
+                    <div class="col-sm-8">
+                        <input type="file">
+                        <p class="help-block">Last opp et bilde av logoen.</p>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">E-post</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control">
+                        <p class="help-block">Kvitteringen sendes til denne adressen.</p>
+                    </div>
+                </div>
+
+            </form>
+        </div>
