9a2f39c0434bfce9347b438544faa1e51db92645
Michael McMillan <email@michaelmcmillan.net>
2014-11-09 17:27:19 +0100

    fixed thanks page for submitjob

--- app/views/submit/thanks.php
+++ app/views/submit/thanks.php
@@ -1,1 +1,10 @@
-ey takk mann
+<div id="about" class="col-md-8 col-md-offset-2">
+
+    <div class="intro">
+        <h3>Annonsen er motatt!</h3>
+        <p>
+            Vi vil så snart som mulig publisere annonsen din. Faktura er sendt til
+            den oppgitte e-postadressen.
+        </p>
+    </div>
+</div>
