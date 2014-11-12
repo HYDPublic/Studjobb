efb117be69463c86679ffd30fc32e6267945780e
Michael McMillan <email@michaelmcmillan.net>
2014-11-12 09:08:19 +0100

    feed to exclude 7 days < old jobs

--- app/controllers/feed.php
+++ app/controllers/feed.php
@@ -5,7 +5,10 @@ class Feed {
         $app = \Slim\Slim::getInstance();
 
         $app->response->headers->set('Content-Type', 'text/xml');
-        $jobs = Job::where('published', true)->orderBy('created_at', 'DESC')->get();
+        $jobs = Job::where('published', true)
+                ->whereRaw('created_at >= now() - interval 1 week')
+                ->orderBy('created_at', 'DESC')
+                ->get();
         $app->render('feed.php', array (
             'jobs' => $jobs
         ));
