aa91fccd54357d29052ba8f6620025339c7253d3
Michael McMillan <email@michaelmcmillan.net>
2014-11-12 11:04:31 +0100

    removed week filter for rss feed, seemed to mess things up

--- app/controllers/feed.php
+++ app/controllers/feed.php
@@ -6,7 +6,6 @@ class Feed {
 
         $app->response->headers->set('Content-Type', 'text/xml');
         $jobs = Job::where('published', true)
-                ->whereRaw('created_at >= now() - interval 1 week')
                 ->orderBy('created_at', 'DESC')
                 ->get();
         $app->render('feed.php', array (
