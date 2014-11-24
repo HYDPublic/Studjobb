fac7edaa4b2af42212e2bc5e34fca7378d47ea26
Michael McMillan <email@michaelmcmillan.net>
2014-11-24 12:58:31 +0100

    git fetch & reset on webhook

--- app/controllers/deploy.php
+++ app/controllers/deploy.php
@@ -3,6 +3,7 @@ class Deploy {
 
     public function pull () {
         $app = \Slim\Slim::getInstance();
-        
+        $payload = json_decode ($app->request->getBody(), true);
+        shell_exec ('cd /var/www/studjobb; git fetch --all; git reset --hard origin/development');
     }
 }
