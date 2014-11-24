9772e6b3533ca09b041b573b7bf98159842fafc7
Michael McMillan <email@michaelmcmillan.net>
2014-11-24 13:09:25 +0100

    debug mode

--- app/controllers/deploy.php
+++ app/controllers/deploy.php
@@ -4,6 +4,7 @@ class Deploy {
     public function pull () {
         $app = \Slim\Slim::getInstance();
         $payload = json_decode ($app->request->getBody(), true);
-        shell_exec ('cd /var/www/studjobb; git fetch --all; git reset --hard origin/development');
+        echo shell_exec ('cd /var/www/studjobb; git fetch --all; git reset --hard origin/development');
+        echo shell_exec('whoami');
     }
 }
