3c236c062fd2a4779c1b5d376e79063bdc28e26f
Michael McMillan <email@michaelmcmillan.net>
2014-11-24 10:07:40 +0100

    replace quill with textarea to avoid collision

--- app/views/admin/edit-crawledJob.php
+++ app/views/admin/edit-crawledJob.php
@@ -79,27 +79,11 @@
                                 <input type="text" name="logo" class="form-control">
                             </div>
 
-                            <div id="formatting-container">
-                                <select title="Size" class="ql-size">
-                                    <option value="10px">Small</option>
-                                    <option value="13px" selected>Normal</option>
-                                    <option value="18px">Large</option>
-                                    <option value="32px">Huge</option>
-                                </select>
-
-                                <button type="button" class="btn ql-bold">Bold</button>
-                                <button type="button" class="btn ql-italic">Italic</button>
-                                <button type="button" class="btn ql-underline">Under</button>
-                                <button type="button" class="btn ql-strike">Strike</button>
-                                <button type="button" class="btn ql-link">Link</button>
-                                <button type="button" class="btn ql-image">Image</button>
-                                <button type="button" class="btn ql-bullet">Bullet</button>
-                                <button type="button" class="btn ql-list">List</button>
+                            <div class="form-group">
+                                <label>Om selskapet</label>
+                                <textarea name="about" class="form-control"></textarea>
                             </div>
 
-                            <div id="editor-container"></div>
-
-                            <textarea id="buffer" name="about"></textarea>
 
                             <input type="submit" class="btn btn-custom-lighten" value="Lagre">
                         </form>
