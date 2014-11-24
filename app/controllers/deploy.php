fc792e23c1924541f04dde91b7f7f5a38f20e383
Michael McMillan <email@michaelmcmillan.net>
2014-11-24 15:44:08 +0100

    only deploy if the hmac is correctly signed (payload) from github

--- app/controllers/deploy.php
+++ app/controllers/deploy.php
@@ -6,14 +6,10 @@ class Deploy {
 
         $sign = substr($app->request->headers->get('X-Hub-Signature'), 5);
 		$hmac = hash_hmac('sha1', $app->request->getBody(), $app->github['deployKey']);
-		if ($sign == $hmac)
-            echo 'Signed!!' . $app->request->headers->get('X-Hub-Signature');
-        else
-            echo "not signed";
 
-        $payload = json_decode ($app->request->getBody(), true);
-        echo shell_exec ('cd /var/www/studjobb/scripts && ./deploy.sh');
-        echo shell_exec('whoami');
-
+        if ($sign == $hmac) {
+            echo shell_exec ('cd /var/www/studjobb/scripts && ./deploy.sh');
+            echo shell_exec('whoami');
+        }
     }
 }
