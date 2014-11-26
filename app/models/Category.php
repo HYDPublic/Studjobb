1acb266e7443a1eef6fcc8998f02b03b9fb9806d
Michael McMillan <email@michaelmcmillan.net>
2014-11-26 08:58:17 +0100

    show jobs, even if they are past date

--- app/models/Category.php
+++ app/models/Category.php
@@ -10,7 +10,7 @@ class Category extends Eloquent {
 		return $this
             ->hasMany('Job')
             ->where('published', true)
-            ->where('due', '>', date('Y-m-d G:i:s'))
+            //->where('due', '>', date('Y-m-d G:i:s'))
             ->whereRaw('created_at >= now() - interval 30 day')
             ->orderBy('jobs.created_at', 'desc');
 	}
