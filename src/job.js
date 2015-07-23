ada2c6682fb11a2bba37f4b09fd9561af44176d9
Michael McMillan <email@michaelmcmillan.net>
2015-07-23 02:31:22 +0200

    added title and application due date with test to Job class

--- src/job.js
+++ src/job.js
@@ -1,9 +1,45 @@
 'use strict';
 
-class job {
-    constructor () {
+class Job {
 
+    constructor () {
+        this._title = 'Mangler tittel.';
+        this._description = 'Mangler beskrivelse.';
+        this._applicationDueDate = this.generateDateTwoWeeksFromNow();
+    }
+
+    get title () {
+        return this._title;
+    }
+
+    set title (title) {
+        title = this.capitalizeFirstCharacter(title);
+        this._title = title;
+    }
+
+    get description () {
+        return this._description;
+    }
+
+    get applicationDueDate () {
+        return this._applicationDueDate; 
+    }
+
+    set applicationDueDate (applicationDueDate) {
+        this._applicationDueDate = applicationDueDate;
+    }
+
+    get expired () {
+        return (new Date() > this._applicationDueDate);
+    }
+
+    capitalizeFirstCharacter (stringToBeCapitalized) {
+        return stringToBeCapitalized.charAt(0).toUpperCase() + stringToBeCapitalized.slice(1);
+    }
+
+    generateDateTwoWeeksFromNow () {
+        return new Date(+new Date() + 12096e5); 
     }
 }
 
-module.exports = job;
+module.exports = Job;
--- test/job.test.js
+++ test/job.test.js
@@ -1,8 +1,69 @@
 var assert = require('assert');
-var job    = require('../src/job.js');
+var Job    = require('../src/job.js');
 
 describe('Job', function () {
-    it('', function () {
+
+    describe('title', function () {
+        
+        it('has a default value', function () {
+            var job = new Job(); 
+            assert.equal(typeof job.title, 'string');
+        });
+
+        it('can be set', function () {
+            var job = new Job(); 
+            job.title = 'Programmerer søkes!';
+            assert.equal(job.title, 'Programmerer søkes!');
+        });
+
+        it('should have first character capitalized', function () {
+            var job = new Job(); 
+            job.title = 'programmerer søkes!';
+            assert.equal(job.title, 'Programmerer søkes!');
+        });
+
+    });
+
+    describe('Application due date', function () {
+
+        it('should by default be set to 2 weeks in the future', function () {
+            var job = new Job();
+            var now = new Date();
+            var twoWeeksInMilliseconds = 12096e5 - 50;
+            assert.equal(job.applicationDueDate - now > twoWeeksInMilliseconds, true);
+        });
     
+        it('can be set to another date', function () {
+            var job = new Job();
+            var tomorrow = new Date(+new Date() + 864e5);
+            job.applicationDueDate = tomorrow;
+            assert.equal(job.applicationDueDate, tomorrow);
+        });
+
+        it('should be expired if set to date in the past', function () {
+            var job = new Job();
+            var now = new Date();
+            var yesterday = new Date(+new Date() - 864e5); 
+            job.applicationDueDate = yesterday;
+            assert.equal(job.expired, true);
+        });
+
+        it('should not be expired if set to date in the future', function () {
+            var job = new Job();
+            var now = new Date();
+            var tomorrow = new Date(+new Date() + 864e5); 
+            assert.equal(job.expired, false);
+        });
+    });
+
+    describe('Description', function () {
+        
+        it('must have a default description', function () {
+            assert.equal(typeof new Job().description, 'string');
+        });
+
+        xit('should be markdown compatible', function () {
+
+        });
     });
 });
