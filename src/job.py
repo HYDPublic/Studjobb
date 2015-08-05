2aec8849c4e9e44a99f47143a6d662d0c81e0497
Michael McMillan <email@michaelmcmillan.net>
2015-08-05 14:30:06 +0200

    Added more methods to Job that deals with title + tests

--- src/job.py
+++ src/job.py
@@ -1,5 +1,25 @@
-class Job():
+class TitleException(Exception):
+    pass
+
+class Job(object):
 
     def __init__(self, title):
-        self.title = title.strip().capitalize()
-        pass
+        self.title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        formattedTitle = Job.formatTitle(title)
+        if (len(formattedTitle) <= 5):
+            raise TitleException('Title must be longer than 5 characters.')
+        self._title = formattedTitle
+
+    @staticmethod
+    def formatTitle(title):
+        title = title[0].title() + title[1:] 
+        return title \
+            .strip() \
+            .replace('\n', ' ')
--- tests/job_title_test.py
+++ tests/job_title_test.py
@@ -1,13 +1,26 @@
 import unittest 
 from src.job import Job 
+from src.job import TitleException 
 
 class TestJob(unittest.TestCase):
 
     def test_title_capitalized(self):
         job = Job("javascript-programmer")
-        assert job.title == "Javascript-programmer"
+        self.assertEqual(job.title, "Javascript-programmer")
 
     def test_title_trimmed(self):
         job = Job("  Javascript-programmer   ")
-        assert job.title == "Javascript-programmer"
+        self.assertEqual(job.title, "Javascript-programmer")
+
+    def test_title_should_not_lowercase_other_characters(self):
+        job = Job("Javascript-programmer at DNB")
+        self.assertEqual(job.title, "Javascript-programmer at DNB")
+        
+
+    def test_title_removes_linebreaks(self):
+        job = Job("Javascript-programmer\nWith angular experience!")
+        self.assertEqual(job.title, "Javascript-programmer With angular experience!")
+
+    def test_title_must_be_longer_than_5_characters(self):
+        self.assertRaises(TitleException, Job, 'title')
 
