27f0a4e844b783d61534cd37843a72ee706e1722
Michael McMillan <email@michaelmcmillan.net>
2015-08-07 16:31:16 +0200

    default case for sorting

--- tests/board/board_sort_test.py
+++ tests/board/board_sort_test.py
@@ -5,6 +5,11 @@ from src.job.job     import Job
 
 class TestBoardSorting(unittest.TestCase):
 
+    def test_board_does_not_sort_by_default(self):
+        jobs = [Job(title="xxxxxx"), Job(title="bbbbbb"), Job(title="aaaaaa")]
+        board = Board(jobs = jobs)
+        self.assertEqual(board.jobs[0].title, "Xxxxxx")
+
     def test_board_can_sort_jobs_by_title(self):
         jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
         board = Board(jobs = jobs, sortByAttribute = "title")
