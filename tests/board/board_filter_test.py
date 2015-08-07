9bc0f8e58add6cb339fae2b174808cbbf9c0273d
Michael McMillan <email@michaelmcmillan.net>
2015-08-07 15:45:56 +0200

    make filtering expired jobs optional

--- tests/board/board_filter_test.py
+++ tests/board/board_filter_test.py
@@ -7,6 +7,6 @@ from src.job.job     import Job
 class TestBoardFilter(unittest.TestCase):
 
     def test_board_can_filter_out_expired_jobs(self):
-        jobs = [Job(title="Aaaaaa", due_date=datetime.date(1993, 4, 20))]
-        board = Board(jobs = jobs)
-        self.assertEqual(board.jobs[0].title, "Aaaaaa")
+        jobs = [Job(due_date=datetime.date(1993, 4, 20))]
+        board = Board(jobs = jobs, filterExpiredJobs = True)
+        self.assertEqual(len(board.jobs), 0)
