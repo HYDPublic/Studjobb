1127ab4b7b3c302f5b8bd13463aa6e3bbe36fe16
Michael McMillan <email@michaelmcmillan.net>
2015-08-07 13:36:14 +0200

    sort methods with tests for board

--- src/board/board.py
+++ src/board/board.py
@@ -1,2 +1,21 @@
-class Board():
-    pass
+# -*- coding: utf-8 -*-
+
+class Board(object):
+
+    sorters = {
+        'title':    lambda job: job.title,
+        'due_date': lambda job: job.due_date,
+    }
+
+    def __init__(self, jobs = [], sortBy = None):
+        self._sortBy = sortBy
+        self._jobs = jobs
+        pass
+
+    @property
+    def jobs(self):
+        if self._sortBy is not None and self._sortBy in Board.sorters:
+            self._jobs.sort(key=Board.sorters[self._sortBy])
+
+        return self._jobs 
+
--- tests/board/board_test.py
+++ tests/board/board_test.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
 import unittest 
 from src.board.board import Board
+from src.job.job     import Job 
 
 class TestBoard(unittest.TestCase):
+    
+    def test_board_has_zero_jobs_by_default(self):
+        self.assertEqual(len(Board().jobs), 0)
+    
+    def test_board_can_set_jobs_in_constructor(self):
+        jobs = [Job(), Job(), Job()]
+        board = Board(jobs = jobs)
+        self.assertEqual(len(board.jobs), 3)
 
-    def test_title_capitalized(self):
-        pass
+    def test_board_can_sort_jobs_by_title(self):
+        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
+        board = Board(jobs = jobs, sortBy = "title")
+        self.assertEqual(board.jobs[0].title, "Aaaaaa")
+
+    def test_board_ignores_sort_if_attribute_does_not_exist(self):
+        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
+        board = Board(jobs = jobs, sortBy = "non_existent_attribute")
+        self.assertEqual(board.jobs[0].title, "Cccccc")
+
