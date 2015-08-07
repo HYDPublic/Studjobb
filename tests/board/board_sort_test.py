143dbd212f46ba89f27f0e268d21e4101d01cdcd
Michael McMillan <email@michaelmcmillan.net>
2015-08-07 15:30:03 +0200

    separated sort tests

---
+++ tests/board/board_sort_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import unittest 
+from src.board.board import Board
+from src.job.job     import Job 
+
+class TestBoardSorting(unittest.TestCase):
+
+    def test_board_can_sort_jobs_by_title(self):
+        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
+        board = Board(jobs = jobs, sortBy = "title")
+        self.assertEqual(board.jobs[0].title, "Aaaaaa")
+
+    def test_board_ignores_sort_if_attribute_does_not_exist(self):
+        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
+        board = Board(jobs = jobs, sortBy = "non_existent_attribute")
+        self.assertEqual(board.jobs[0].title, "Cccccc")
--- tests/board/board_test.py
+++ tests/board/board_test.py
@@ -13,12 +13,3 @@ class TestBoard(unittest.TestCase):
         board = Board(jobs = jobs)
         self.assertEqual(len(board.jobs), 3)
 
-    def test_board_can_sort_jobs_by_title(self):
-        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
-        board = Board(jobs = jobs, sortBy = "title")
-        self.assertEqual(board.jobs[0].title, "Aaaaaa")
-
-    def test_board_ignores_sort_if_attribute_does_not_exist(self):
-        jobs = [Job(title="cccccc"), Job(title="bbbbbb"), Job(title="aaaaaa")]
-        board = Board(jobs = jobs, sortBy = "non_existent_attribute")
-        self.assertEqual(board.jobs[0].title, "Cccccc")
