8e2966f6f0cae5b96efef63d646bbb18db1a7f74
Michael McMillan <email@michaelmcmillan.net>
2015-08-09 13:15:54 +0200

    use flask for serving web

---
+++ src/webserver/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__, template_folder='views/', static_folder='assets')
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+if __name__ == '__main__':
+    app.run()
