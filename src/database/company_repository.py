d045437eb791ff6e17d74a0a150fd2ccf8a02784
Michael McMillan <email@michaelmcmillan.net>
2015-08-10 17:40:12 +0200

    make job_repository fetch company

---
+++ src/database/company_repository.py
@@ -0,0 +1,26 @@
+from src.company.company import Company
+
+class CompanyRepository(object):
+
+    def __init__(self, database):
+        self._database = database
+        self._table = 'companies'
+
+    def find(self, id):
+        result = self._database.execute('select * from %s where id = %d' % (self._table, id))
+        row = result.fetchone()
+        company = Company(name = row.name)
+        return company 
+
+    def findAll(self):
+        result = self._database.execute('select * from %s' % (self._table))
+        companies = []
+        for row in result:
+            companies.append(Company(name = row.name))
+        return companies 
+
+    def save(self, company):
+        pass
+
+    def remove(self, company):
+        pass
--- src/database/job_repository.py
+++ src/database/job_repository.py
@@ -1,22 +1,26 @@
-from src.job.job import Job
+from src.job.job        import Job
+from company_repository import CompanyRepository 
 
 class JobRepository(object):
 
     def __init__(self, database):
         self._database = database
         self._table = 'jobs'
+        self._companyRepository = CompanyRepository(database)
 
     def find(self, id):
         result = self._database.execute('select * from %s where id = %d' % (self._table, id))
         row = result.fetchone()
-        job = Job(id = row.id, title = row.title, description = row.description, due_date = row.due_date)
+        company = self._companyRepository.find(row.company_id)
+        job = Job(id = row.id, title = row.title, description = row.description, due_date = row.due_date, company = company)
         return job
 
     def findAll(self):
         result = self._database.execute('select * from %s' % (self._table))
         jobs = []
         for row in result:
-            jobs.append(Job(id = row.id, title = row.title, description = row.description, due_date = row.due_date))
+            company = self._companyRepository.find(row.company_id)
+            jobs.append(Job(id = row.id, title = row.title, description = row.description, due_date = row.due_date, company = company))
         return jobs
 
     def save(self, job):
