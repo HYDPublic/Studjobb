d9c91c1c8372f56933ae46e232e1c49f1f98a974
Michael McMillan <email@michaelmcmillan.net>
2015-08-10 16:42:52 +0200

    import repositories and define error handler and board controller

--- src/webserver/main.py
+++ src/webserver/main.py
@@ -1,10 +1,27 @@
+import sys
+sys.path.append('/Users/michaelmcmillan/Prosjekter/Studjobb')
 from flask import Flask
 from flask import render_template
-app = Flask(__name__, template_folder='views/', static_folder='assets')
+from src.database.engine import database
+from src.database.job_repository import JobRepository
+from src.database.board_repository import BoardRepository
+    
+# Configuration
+app = Flask(__name__, template_folder = 'views', static_folder = 'assets')
 
+# Repositories
+job_repository = JobRepository(database)
+board_repository = BoardRepository(database)
+
+# Routes
 @app.route('/')
-def index():
-    return render_template('index.html') 
+def board():
+    board = board_repository.find()
+    return render_template('board.html', jobs = board.jobs) 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
