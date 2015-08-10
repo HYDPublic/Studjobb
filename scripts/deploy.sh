cfb8dd9be8a35489fb71796b2317a2a919bc0f4c
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 01:51:48 +0200

    recursively upload entire dir to server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 ssh -o "StrictHostKeyChecking no" -i scripts/id_rsa michael@studjobb.no "ifconfig > ~/hey.txt"
-scp -o "StrictHostKeyChecking no" -i scripts/id_rsa -r scripts/deploy.sh michael@studjobb.no:/home/michael/
+scp -o "StrictHostKeyChecking no" -i scripts/id_rsa -r . michael@studjobb.no:/home/michael/
