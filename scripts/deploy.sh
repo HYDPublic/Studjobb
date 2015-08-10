fb74676b51bbf470cc699f4e13bb8ce83d470b99
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 01:53:10 +0200

    upload to /srv

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,1 @@
-ssh -o "StrictHostKeyChecking no" -i scripts/id_rsa michael@studjobb.no "ifconfig > ~/hey.txt"
-scp -o "StrictHostKeyChecking no" -i scripts/id_rsa -r . michael@studjobb.no:/home/michael/
+scp -o "StrictHostKeyChecking no" -i scripts/id_rsa -r . michael@studjobb.no:/srv/studjobb.no
