04c8407adff5b2a64240b78345aa835d6cf9f00f
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 13:01:10 +0200

    use rsync instead of scp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
-scp -o "StrictHostKeyChecking no" -i scripts/id_rsa --exclude .git --exclude config -r . michael@studjobb.no:/srv/studjobb.no
+rsync -Pav -e 'ssh -o StrictHostKeyChecking=no -i scripts/id_rsa -C -c blowfish' . michael@studjobb.no:/srv/studjobb.no
+
+#scp -o "StrictHostKeyChecking no" -i scripts/id_rsa --exclude .git --exclude config -r . michael@studjobb.no:/srv/studjobb.no
