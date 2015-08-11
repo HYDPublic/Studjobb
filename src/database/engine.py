adaac9b0e2e1a518e699c569072a4109c38cc9d2
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 13:13:17 +0200

    use config when connecting to db in engine

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,3 +1,13 @@
-from sqlalchemy import create_engine
+import os
+from sqlalchemy   import create_engine
+from ConfigParser import SafeConfigParser
 
-database = create_engine('mysql://root@localhost/studjobb_rewrite?charset=utf8')
+config = SafeConfigParser()
+config.read(os.path.abspath(os.path.join(__file__, '..', '..', '..', 'config')))
+
+host     = config.get("database", "host")
+user     = config.get("database", "user")
+password = config.get("database", "password")
+name     = config.get("database", "name")
+
+database = create_engine('mysql://%s:%s@%s/%s?charset=utf8' % (user, password, host, name))
