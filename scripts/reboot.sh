cda455094f8ae08b85aae5634426e0ef2788181b
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 15:10:34 +0200

    ignore kill signal and pipe stdout/stderr to /dev/null

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,1 +1,1 @@
-kill -9 $(pidof python); nohup python /srv/studjobb.no/src/webserver/webserver.py &
+kill -9 $(pidof python) || true; nohup python /srv/studjobb.no/src/webserver/webserver.py > /dev/null 2>&1 &
