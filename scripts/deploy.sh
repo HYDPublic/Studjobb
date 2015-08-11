641712652d9be3ec8bcd6011ce97bd35c472d63c
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 13:03:01 +0200

    exclude config and .git

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,1 @@
-rsync -Pav -e 'ssh -o StrictHostKeyChecking=no -i scripts/id_rsa -C -c blowfish' . michael@studjobb.no:/srv/studjobb.no
-
-#scp -o "StrictHostKeyChecking no" -i scripts/id_rsa --exclude .git --exclude config -r . michael@studjobb.no:/srv/studjobb.no
+rsync --exclude='.git/' --exclude='config'  -Pav -e 'ssh -o StrictHostKeyChecking=no -i scripts/id_rsa -C -c blowfish' . michael@studjobb.no:/srv/studjobb.no
