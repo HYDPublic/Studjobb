d69b29cdc5badd6277d1f01aa63d37a784ef7fda
Michael McMillan <email@michaelmcmillan.net>
2015-08-11 12:53:35 +0200

    exclude deployment of git dir and config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-scp -o "StrictHostKeyChecking no" -i scripts/id_rsa -r . michael@studjobb.no:/srv/studjobb.no
+scp --exclude '.git' --exclude 'config' -o "StrictHostKeyChecking no" -i scripts/id_rsa -r . michael@studjobb.no:/srv/studjobb.no
