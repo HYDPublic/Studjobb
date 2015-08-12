6a457919044561415d55c96626be132865313b5b
Michael McMillan <email@michaelmcmillan.net>
2015-08-12 18:29:08 +0200

    test for setting start_date to None

--- tests/job/start_date_test.py
+++ tests/job/start_date_test.py
@@ -10,6 +10,10 @@ class TestJobStartDate(unittest.TestCase):
     def test_job_has_none_as_default_start_date(self):
         self.assertEqual(Job().start_date, None)
 
+    def test_job_start_date_can_be_set_to_none(self):
+        job = Job()
+        job.start_date = None
+
     def test_job_start_date_can_be_provided_as_a_ddmmyyy_string(self):
         job = Job(start_date = '2017-09-29')
         self.assertEqual(job.start_date, datetime.date(2017, 9, 29))
