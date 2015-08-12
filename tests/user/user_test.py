f17d1bcf4aec4d785fb4c2172ea995847ebcc6c8
Michael McMillan <email@michaelmcmillan.net>
2015-08-12 14:48:34 +0200

    Moved user into separate dir even though it extends Person

--- tests/user/user_test.py
+++ tests/user/user_test.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest 
 import mock
-from src.person.user.user import User 
-from src.person.user.user import UserException
 
+from src.user.user import User 
+from src.user.user import UserException
 
 class TestUser(unittest.TestCase):
     
@@ -13,7 +13,7 @@ class TestUser(unittest.TestCase):
     def test_user_raises_exception_if_password_is_not_provided(self):
         self.assertRaisesRegexp(UserException, "password", User, email = "h@h")
 
-    @mock.patch("src.person.user.user.bcrypt")
+    @mock.patch("src.user.user.bcrypt")
     def test_user_credentials_are_valid_if_correct_password_is_supplied(self, mock_bcrypt):
         hashed_password = '$2a$10$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi'
         mock_bcrypt.hashpw.return_value = hashed_password
@@ -21,7 +21,7 @@ class TestUser(unittest.TestCase):
         valid_credentials = user.checkCredentials(plain_text_password = "abc")
         self.assertEqual(valid_credentials, True)
 
-    @mock.patch("src.person.user.user.bcrypt")
+    @mock.patch("src.user.user.bcrypt")
     def test_user_credentials_are_invalid_if_incorrect_password_is_supplied(self, mock_bcrypt):
         hashed_password = '$2a$10$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi'
         mock_bcrypt.hashpw.return_value = "not-the-same-hash"
