f154ebdce47a63740a92f7c8086b1da9fba11799
Michael McMillan <email@michaelmcmillan.net>
2015-08-17 19:02:56 +0200

    add more allowed tags

--- src/job/description.py
+++ src/job/description.py
@@ -8,7 +8,8 @@ class Description(object):
     validHTMLTags = [
         'h1', 'h2', 'h3', 'h4', 'h5',
         'b', 'i', 'em', 'p', 'img',
-        'ul', 'li', 'a', 'strong', 'div'
+        'ul', 'li', 'a', 'strong', 'div',
+        'br', 'span', 'form'
     ]
 
     @staticmethod
