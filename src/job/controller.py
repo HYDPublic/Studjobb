64601bb1a04d2f8fd181b0c1a0637161af8dd914
Michael McMillan <email@michaelmcmillan.net>
2015-08-19 14:28:46 +0200

    controller class for job

---
+++ src/job/controller.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class JobController(object):
+
+    def create(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+JobController().create()
