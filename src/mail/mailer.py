466ba19b2060bde42095d2ce4763547ffce6a9c0
Michael McMillan <email@michaelmcmillan.net>
2015-09-17 10:21:20 +0200

    start over

--- src/mail/mailer.py
+++ src/mail/mailer.py
@@ -1,19 +1,6 @@
-import smtplib 
-
 class Mailer(object):
 
-    def __init__(self):
-        self.server = smtplib.SMTP()
-        
     def send(self, mail):
         pass
 
-    def connect_to_server(self):
-        server.connect(server, port)
-        server.ehlo()
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(send_from, send_to, msg)
-        server.close()
 
-        
