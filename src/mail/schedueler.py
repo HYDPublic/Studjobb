d7ab93e5ea5a83ef0667935b0e985d0eda7bd1c0
Michael McMillan <email@michaelmcmillan.net>
2015-09-29 11:54:04 +0200

    simplified the schedueler class. dequeue/enqueue removed

--- src/mail/schedueler.py
+++ src/mail/schedueler.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import smtplib
 import datetime
 from mailer import Mailer 
 from schedueled_entry import SchedueledEntry
@@ -9,31 +10,37 @@ class ScheduelerError(Exception):
 class Schedueler(object):
 
     def __init__(self, queue = None):
-        self._mailer = Mailer()
-        self._queue  = queue or []
+        self._mailer    = Mailer()
+        self._queue     = queue or []
+        self.schedueled_entries_that_were_sent = []
 
     @property
     def queue(self):
         return self._queue
     
-    def enqueue(self, mail, when):
-        schedueled_entry = SchedueledEntry(mail, when)
-        self._queue.append(schedueled_entry)
-
-    def dequeue(self, now = datetime.datetime.now()):
+    def run(self, now = datetime.datetime.now()):
         for schedueled_mail in self.mails_to_be_sent_now(now = now):
-            index = self._queue.index(schedueled_mail)
-            mail  = self._queue.pop(index)
-            self.send(mail);
-
-    def remove_duplicates(self):
-        self._queue = list(set(self._queue))
+            index            = self._queue.index(schedueled_mail)
+            schedueled_entry = self._queue.pop(index)
+            self.send(schedueled_entry);
 
     def mails_to_be_sent_now(self, now = datetime.datetime.now()):
-        return filter(lambda schedueled_entry: schedueled_entry.when < now, self._queue)
+        return filter(lambda schedueled_entry: schedueled_entry.when < now and schedueled_entry.sent == False, self._queue)
 
-    def send(self, mail):
+    def mark_as_sent(self, schedueled_entry):
+        schedueled_entry.sent = True
+        self.schedueled_entries_that_were_sent.append(schedueled_entry)
+
+    def connect(self):
+        self._mailer.connect()
+        self._mailer.authenticate()
+
+    def disconnect(self):
+        self._mailer.disconnect()
+
+    def send(self, schedueled_entry):
         try:
-            self._mailer.send(mail)
-        except Exception:
-            self._queue.append(mail)
+            self._mailer.send(schedueled_entry.mail)
+            self.mark_as_sent(schedueled_entry)
+        except smtplib.SMTPException:
+            self._queue.append(schedueled_entry)
--- tests/mail/schedueler_test.py
+++ tests/mail/schedueler_test.py
@@ -2,6 +2,7 @@
 import mock
 import unittest 
 import datetime
+import smtplib
 from src.mail.mail import Mail 
 from src.mail.schedueler import Schedueler 
 from src.mail.schedueled_entry import SchedueledEntry 
@@ -16,50 +17,43 @@ class TestSchedueler(unittest.TestCase):
         schedueled_entry = SchedueledEntry(FakeMail(), datetime.datetime(2016, 01, 01))
         self.assertEqual(schedueled_entry.sent, False);
 
-    def test_schedueler_can_enqueue_a_single_mail(self):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = None)
-        self.assertEqual(len(schedueler.queue), 1)
-
-    def test_schedueler_can_enqueue_multiple_mails(self):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = None)
-        schedueler.enqueue(mail = FakeMail(), when = None)
-        schedueler.enqueue(mail = FakeMail(), when = None)
-        self.assertEqual(len(schedueler.queue), 3)
-
-    def test_schedueler_can_remove_duplicate_mails(self):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(id = 1), when = None)
-        schedueler.enqueue(mail = FakeMail(id = 1), when = None)
-        schedueler.remove_duplicates()
-        self.assertEqual(len(schedueler.queue), 1)
-
     def test_schedueler_extracts_one_mail_if_its_scheduele_time_is_in_the_past(self):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = datetime.datetime(2015, 01, 01, 8, 30))
+        schedueled_entries = [SchedueledEntry(mail = FakeMail(), when = datetime.datetime(2015, 01, 01, 8, 30))]
+        schedueler = Schedueler(schedueled_entries)
         mock_now = datetime.datetime(2016, 01, 01, 9, 0)
         self.assertEqual(len(schedueler.mails_to_be_sent_now(now = mock_now)), 1)
 
     def test_schedueler_extracts_no_mail_if_its_scheduele_time_is_in_the_future(self):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = datetime.datetime(2016, 01, 01, 9, 0))
+        schedueled_entries = [SchedueledEntry(mail = FakeMail(), when = datetime.datetime(2016, 01, 01, 8, 30))]
+        schedueler = Schedueler(schedueled_entries)
         mock_now = datetime.datetime(2015, 01, 01, 8, 30)
         self.assertEqual(len(schedueler.mails_to_be_sent_now(now = mock_now)), 0)
 
+    def test_schedueler_extracts_no_mail_if_its_already_marked_as_sent(self):
+        past = datetime.datetime(2000, 01, 01, 12, 0)
+        schedueler = Schedueler([SchedueledEntry(mail = FakeMail(), sent = True, when = past)])
+        mock_now = datetime.datetime(2015, 01, 01, 8, 30)
+        self.assertEqual(len(schedueler.mails_to_be_sent_now(now = mock_now)), 0)
+
+    def test_schedueler_extracts_mail_if_its_not_marked_as_sent(self):
+        past = datetime.datetime(2000, 01, 01, 12, 0)
+        schedueler = Schedueler([SchedueledEntry(mail = FakeMail(), sent = False, when = past)])
+        mock_now = datetime.datetime(2015, 01, 01, 8, 30)
+        self.assertEqual(len(schedueler.mails_to_be_sent_now(now = mock_now)), 1)
+
     @mock.patch('src.mail.mailer.Mailer.send')
     def test_schedueler_clears_sent_jobs_after_run(self, mock_mailer):
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = datetime.datetime(2016, 01, 01,  9, 0))
-        schedueler.dequeue(now = datetime.datetime(2018, 01, 01, 8, 30))
+        schedueled_entries = [SchedueledEntry(mail = FakeMail(), when = datetime.datetime(2016, 01, 01,  9, 0))]
+        schedueler = Schedueler(schedueled_entries)
+        schedueler.run(now = datetime.datetime(2018, 01, 01, 8, 30))
         self.assertEqual(len(schedueler.queue), 0)
 
     @mock.patch('src.mail.mailer.Mailer.send')
     def test_schedueler_does_not_clear_jobs_if_sender_raises_exception(self, mock_sender):
-        mock_sender.side_effect = Exception('Mail could not be sent.')
-        schedueler = Schedueler()
-        schedueler.enqueue(mail = FakeMail(), when = datetime.datetime(2016, 01, 01,  9, 0))
-        schedueler.dequeue(now = datetime.datetime(2018, 01, 01, 8, 30))
+        mock_sender.side_effect = smtplib.SMTPException('Mail could not be sent.')
+        schedueled_entries = [SchedueledEntry(mail = FakeMail(), when = datetime.datetime(2016, 01, 01,  9, 0))]
+        schedueler = Schedueler(schedueled_entries)
+        schedueler.run(now = datetime.datetime(2018, 01, 01, 8, 30))
         self.assertEqual(len(schedueler.queue), 1)
 
     def test_schedueler_can_take_schedueler_entries_in_constructor(self):
