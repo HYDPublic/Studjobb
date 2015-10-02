c3f1c3209091bf135506d3054d9c44ea1b827ff3
Michael McMillan <email@michaelmcmillan.net>
2015-10-02 10:41:22 +0200

    use ignore_time function to remove hour and min from date when testing date only

--- tests/mail/best_send_date_test.py
+++ tests/mail/best_send_date_test.py
@@ -3,7 +3,7 @@ import unittest
 import datetime
 from src.mail.best_send_date import BestSendDate 
 
-def remove_time(date):
+def ignore_time(date):
     return date.replace(hour=0, minute=0, second=0)
 
 class TestBestSendDate(unittest.TestCase):
@@ -28,24 +28,24 @@ class TestBestSendDate(unittest.TestCase):
     def test_returns_monday_if_currently_sunday(self):
         sunday = datetime.datetime(2015, 10, 04)
         monday = datetime.datetime(2015, 10, 05)
-        self.assertEqual(remove_time(BestSendDate(now = sunday).date), monday)
+        self.assertEqual(ignore_time(BestSendDate(now = sunday).date), monday)
 
     def test_returns_monday_if_currently_saturday(self):
         saturday = datetime.datetime(2015, 10, 03)
         monday   = datetime.datetime(2015, 10, 05)
-        self.assertEqual(remove_time(BestSendDate(now = saturday).date), monday)
+        self.assertEqual(ignore_time(BestSendDate(now = saturday).date), monday)
 
     def test_returns_tuesday_if_currently_monday_three_o_clock(self):
         monday_three_o_clock = datetime.datetime(2015, 10, 05, 15, 00)
         tuesday = datetime.datetime(2015, 10, 06)
-        best_date = BestSendDate(now = monday_three_o_clock).date.replace(hour=0, minute=0, second=0)
-        self.assertEqual(best_date, tuesday)
+        best_date = BestSendDate(now = monday_three_o_clock)
+        self.assertEqual(ignore_time(best_date.date), tuesday)
 
     def test_returns_monday_if_currently_monday_two_o_clock(self):
         monday_two_o_clock = datetime.datetime(2015, 10, 05, 14, 00)
         monday = datetime.datetime(2015, 10, 05)
-        best_date = BestSendDate(now = monday_two_o_clock).date.replace(hour=0, minute=0, second=0)
-        self.assertEqual(best_date, monday)
+        best_date = BestSendDate(now = monday_two_o_clock)
+        self.assertEqual(ignore_time(best_date.date), monday)
 
     def test_returns_friday_if_currently_friday(self):
         friday  = datetime.datetime(2015, 9, 02)
@@ -54,8 +54,8 @@ class TestBestSendDate(unittest.TestCase):
     def test_returns_monday_if_currently_friday_three_o_clock(self):
         friday_three_o_clock = datetime.datetime(2015, 10, 02, 15, 00)
         monday = datetime.datetime(2015, 10, 05)
-        best_date = BestSendDate(now = friday_three_o_clock).date.replace(hour=0, minute=0, second=0)
-        self.assertEqual(best_date, monday)
+        best_date = BestSendDate(now = friday_three_o_clock)
+        self.assertEqual(ignore_time(best_date.date), monday)
 
     def test_time_is_not_adjusted_if_prior_to_threshold_on_a_thursday(self):
         thursday_one_o_clock = datetime.datetime(2015, 10, 01, 13, 13)
