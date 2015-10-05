42471e19701b31027e9ece487fc5360cdca7a526
Michael McMillan <email@michaelmcmillan.net>
2015-10-05 18:48:37 +0200

    account for template subject

--- src/database/template_repository.py
+++ src/database/template_repository.py
@@ -10,13 +10,13 @@ class TemplateRepository(object):
     def find(self, id):
         result = self._database.execute('select * from %s where id = %d' % (self._table, int(id)))
         row = result.fetchone()
-        return Template(id = row.id, title = row.title, text = row.text)
+        return Template(id = row.id, title = row.title, subject = row.subject, text = row.text)
 
     def findAll(self):
         result = self._database.execute('select * from %s' % (self._table))
         templates = []
         for row in result:
-            templates.append(Template(id = row.id, title = row.title, text = row.text))
+            templates.append(Template(id = row.id, title = row.title, subject = row.subject, text = row.text))
         return templates 
 
     def save(self, template):
@@ -26,17 +26,19 @@ class TemplateRepository(object):
             return self.update(template)
 
     def update(self, template):
-        result = self._database.execute(text('update templates set title = :title, text = :text where id = :id'),
-            title = template.title,
-            text =  template.text,
-            id    = template.id 
+        result = self._database.execute(text('update templates set title = :title, subject = :subject, text = :text where id = :id'),
+            title   = template.title,
+            text    = template.text,
+            subject = template.subject,
+            id      = template.id 
         )
         return self.find(template.id) 
 
     def create(self, template):
-        result = self._database.execute(text('insert into templates set title = :title, text = :text'),
-            title = template.title,
-            text  = template.text
+        result = self._database.execute(text('insert into templates set title = :title, subject = :subject, text = :text'),
+            title   = template.title,
+            subject = template.subject,
+            text    = template.text
         )
         template.id = result.lastrowid
         return template
