92ede839ab756ec3a7997bf005f8e489249c065e
Michael McMillan <email@michaelmcmillan.net>
2015-10-05 12:08:37 +0200

    use absolute path to crawler dir and make rsync verbose

--- scripts/push-crawler.sh
+++ scripts/push-crawler.sh
@@ -1,1 +1,4 @@
-rsync -r --exclude='scraped_job.py' ../src/crawler michael@studjobb.no:/srv/studjobb.no/src/
+#!/bin/bash
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
+CRAWLER_DIR=$CURRENT_DIR"/../src/crawler/"
+rsync -r --exclude='scraped_job.py' $CRAWLER_DIR michael@studjobb.no:/srv/studjobb.no/src/ --verbose
