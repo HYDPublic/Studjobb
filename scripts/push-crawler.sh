07c43d9c4cc4684960856c84faed368d9d094521
Michael McMillan <email@michaelmcmillan.net>
2015-10-05 12:21:43 +0200

    remove trailing forward slash

--- scripts/push-crawler.sh
+++ scripts/push-crawler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
-CRAWLER_DIR=$CURRENT_DIR"/../src/crawler/"
+CRAWLER_DIR=$CURRENT_DIR"/../src/crawler"
 rsync -r --exclude='scraped_job.py' $CRAWLER_DIR michael@studjobb.no:/srv/studjobb.no/src/ --verbose
