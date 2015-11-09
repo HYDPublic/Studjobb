14611cd53bcb958ccda14dd0869e5fee2d75a409
Michael McMillan <email@michaelmcmillan.net>
2015-11-09 01:05:32 +0100

    fix up push crawler to only sync formulas

--- scripts/push-crawler.sh
+++ scripts/push-crawler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
-CRAWLER_DIR=$CURRENT_DIR"/../src/crawler"
-rsync -r --exclude='scraped_job.py' $CRAWLER_DIR michael@studjobb.no:/srv/studjobb.no/src/ --verbose
+CRAWLER_DIR=$CURRENT_DIR"/../src/crawler/formulas"
+rsync $CRAWLER_DIR michael@studjobb.no:/srv/studjobb.no/src/ --verbose
