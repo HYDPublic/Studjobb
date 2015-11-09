e9b2049b98b039a337eccf4437674c91275300c7
Michael McMillan <email@michaelmcmillan.net>
2015-11-09 20:02:51 +0100

    remove another unused script

--- scripts/find-proxy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
-CRAWLER_DIR=$CURRENT_DIR"/../src/crawler/"
-python $CRAWLER_DIR"/elite-proxy-finder/elite-proxy-finder.py"
