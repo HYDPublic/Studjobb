12c476dda080b66f4210fa0c5ea45811b76d2cc8
Michael McMillan <email@michaelmcmillan.net>
2015-11-27 10:01:45 +0100

    reuse the code that extracts a job object from an http request

--- src/webserver/controllers/job.py
+++ src/webserver/controllers/job.py
@@ -13,8 +13,20 @@ class JobController(Controller):
         self.company_repository  = CompanyRepository(database) 
         super(JobController, self).__init__()
 
-    def new(self):
+    def get_job_from_request(self, job = None):
+        job = job or Job()
+        job.company     = self.company_repository.find(self.request.form['company'])
+        job.title       = self.request.form.get('title')
+        job.status      = self.request.form.get('status')
+        job.description = self.request.form.get('description')
+        job.due_date    = self.request.form.get('due_date')
+        job.start_date  = self.request.form.get('start_date')
+        job.position    = self.request.form.get('position')
+        job.place       = self.request.form.get('place')
+        job.apply_url   = self.request.form.get('apply_url')
+        return job
 
+    def new(self):
         companies = self.company_repository.findAll()
         return self.render('admin/job/new.html',
             companies = companies,
@@ -27,7 +39,6 @@ class JobController(Controller):
         companies = self.company_repository.findAll()
         if not job:
             return self.abort(404)
-
         return self.render('admin/job/edit.html',
             job        = job,
             companies  = companies,
@@ -38,7 +49,6 @@ class JobController(Controller):
         job = self.job_repository.find(id)
         if not job or job.edit_url != token:
             return self.abort(404)
-
         return self.render('public/job.html', job = job, logged_in = self.user_is_authenticated())
 
     def view(self, id):
@@ -46,38 +56,17 @@ class JobController(Controller):
         published = job.status == 'active'
         if not job or not published:
             return self.abort(404)
-
         return self.render('public/job.html', job = job, logged_in = self.user_is_authenticated())
 
     @Controller.authentication_required
     def create(self):
-        job             = Job() 
-        job.company     = self.company_repository.find(self.request.form['company'])
-        job.title       = self.request.form.get('title')
-        job.place       = self.request.form.get('place')
-        job.due_date    = self.request.form.get('due_date')
-        job.start_date  = self.request.form.get('start_date')
-        job.position    = self.request.form.get('position')
-        job.description = self.request.form.get('description')
-        job.apply_url   = self.request.form.get('apply_url')
-        job = self.job_repository.save(job)
-
-        return self.redirect(self.url_for('job.edit', id = job.id))
+        job = self.get_job_from_request()
+        saved_job = self.job_repository.save(job)
+        return self.redirect(self.url_for('job.edit', id = saved_job.id))
 
     @Controller.authentication_required
     def update(self, id):
         job = self.job_repository.find(id)
-        company = self.company_repository.find(self.request.form['company'])
-
-        job.title       = self.request.form.get('title')
-        job.status      = self.request.form.get('status')
-        job.description = self.request.form.get('description')
-        job.due_date    = self.request.form.get('due_date')
-        job.start_date  = self.request.form.get('start_date')
-        job.position    = self.request.form.get('position')
-        job.place       = self.request.form.get('place')
-        job.apply_url   = self.request.form.get('apply_url')
-        job.company     = company 
-
-        self.job_repository.save(job)
+        updated_job = self.get_job_from_request(job)
+        self.job_repository.save(updated_job)
         return self.redirect(self.url_for('job.edit', id = id))
