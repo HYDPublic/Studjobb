bf295414989d559abd3fbf0939765f8d93bc36c2
Michael McMillan <email@michaelmcmillan.net>
2016-01-04 14:10:24 +0100

    wrongly ignored config module

---
+++ src/config/config.py
@@ -0,0 +1,20 @@
+import os
+from os.path import abspath, join
+from ConfigParser import SafeConfigParser
+
+class Config(object):
+
+    def __init__(self, location):
+        self.parser = SafeConfigParser()
+        self.parser.read(location)
+
+    def get(self, *args, **kwargs):
+        return self.parser.get(*args, **kwargs)
+
+if os.environ.get('TEST'):
+    config_file_name = 'example_config'
+else:
+    config_file_name = 'config'
+
+location = abspath(join(__file__, '..', '..', '..', config_file_name))
+config = Config(location)
