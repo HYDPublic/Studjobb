868a567ea68776cddef67f035495c37ebd3a4db5
Michael McMillan <email@michaelmcmillan.net>
2016-01-07 13:34:18 +0100

    removed example_config fixture completely

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from src.config.config import config
 
-host     = config.get("database", "host")
-user     = config.get("database", "user")
-password = config.get("database", "password")
-name     = config.get("database", "name")
-
-database = create_engine('mysql://%s:%s@%s/%s?charset=utf8' % (user, password, host, name))
+if not os.environ.get('TEST'):
+    host     = config.get("database", "host")
+    user     = config.get("database", "user")
+    password = config.get("database", "password")
+    name     = config.get("database", "name")
+    database = create_engine('mysql://%s:%s@%s/%s?charset=utf8' % (user, password, host, name))
+else:
+    database = None
